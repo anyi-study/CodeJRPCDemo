69a52ed019770aa7111a054796140d9098365c27
CodeJ <jx523644@163.com>
2024-07-23 21:32:01 +0800

    add LocalRegistry、Serializer、Request processor

--- codej-rpc-demo/src/main/java/com/codej/server/VertxHttpServer.java
+++ codej-rpc-demo/src/main/java/com/codej/server/VertxHttpServer.java
@@ -2,29 +2,32 @@ package com.codej.server;
 
 import io.vertx.core.Vertx;
 
-public class VertxHttpServer implements HttpServer{
+/**
+ * Vertx HTTP 服务器
+ */
+public class VertxHttpServer implements HttpServer {
 
-    @Override
+    /**
+     * 启动服务器
+     *
+     * @param port
+     */
     public void doStart(int port) {
-        //创建vertx实例
+        // 创建 Vert.x 实例
         Vertx vertx = Vertx.vertx();
-        //创建http服务器
+
+        // 创建 HTTP 服务器
         io.vertx.core.http.HttpServer server = vertx.createHttpServer();
-        //监听端口并处理请求
-        server.requestHandler(request->{
-            //处理请求
-            System.out.println("Received request:"+request.method()+" "+request.uri());
-//            发送http响应
-            request.response()
-                    .putHeader("content-type","text/plain")
-                    .end("Hello from Vert.x Http server!");
-        });
-//        启动Http服务器并监听指定端口
-        server.listen(port,result->{
-            if (result.succeeded()){
-                System.out.println("Server is now listening on port" + port);
-            }else {
-                System.out.println("Failed to start server:" + result.cause());
+
+        // 监听端口并处理请求
+        server.requestHandler(new HttpServerHandler());
+
+        // 启动 HTTP 服务器并监听指定端口
+        server.listen(port, result -> {
+            if (result.succeeded()) {
+                System.out.println("Server is now listening on port " + port);
+            } else {
+                System.err.println("Failed to start server: " + result.cause());
             }
         });
     }
--- example-provider/src/main/java/com/codej/provider/ProviderExample.java
+++ example-provider/src/main/java/com/codej/provider/ProviderExample.java
@@ -1,5 +1,8 @@
 package com.codej.provider;
 
+import com.codej.common.service.UserService;
+import com.codej.provider.service.impl.UserServiceImpl;
+import com.codej.registry.LocalRegistry;
 import com.codej.server.HttpServer;
 import com.codej.server.VertxHttpServer;
 
@@ -8,6 +11,8 @@ import com.codej.server.VertxHttpServer;
  */
 public class ProviderExample {
     public static void main(String[] args) {
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
 //        提供服务，启动web服务器
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
