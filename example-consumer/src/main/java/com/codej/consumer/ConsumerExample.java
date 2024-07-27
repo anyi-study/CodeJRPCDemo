664a19a65f983b7b695e2dd6421019679f7a71e3
CodeJ <jx523644@163.com>
2024-07-27 19:01:23 +0800

    add dynamic proxy

--- example-consumer/src/main/java/com/codej/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/codej/consumer/ConsumerExample.java
@@ -2,10 +2,14 @@ package com.codej.consumer;
 
 import com.codej.common.model.User;
 import com.codej.common.service.UserService;
+import com.codej.proxy.ServiceProxyFactory;
 
 public class ConsumerExample {
     public static void main(String[] args) {
-        UserService userService = new UserServiceProxy();
+        //静态代理
+        //UserService userService = new UserServiceProxy();
+        // 动态代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
         User user = new User();
         user.setName("rose");
         User newUser = userService.getUser(user);
